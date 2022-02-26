d9594437fec976e7cb735f4fc6ce4cee3cffe721
André Mendes Marcondes <andre.marcondes@betalearning.com.br>
2022-02-26 15:47:41 -0300

    adicionando service

---
+++ CS.Service/Imp/StudentService.cs
@@ -0,0 +1,80 @@
+﻿using CS.Data.Interfaces;
+using CS.Domain;
+using CS.Service.Interface;
+
+namespace CS.Service.Imp
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _repository;
+
+        public StudentService(IStudentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Add(Student student)
+        {
+            _repository.Add(student);
+        }
+
+        public void Delete(Guid id)
+        {
+            _repository.Delete(id);
+        }
+
+        public Student Get(Guid id)
+        {
+            return _repository.Get(id);
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public void Update(Guid id, Student student)
+        {
+            _repository.Update(id, student);
+        }
+
+        public bool IsCpf(string cpf)
+        {
+            if (String.IsNullOrEmpty(cpf))
+                return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+    }
+}
---
+++ CS.Service/Interface/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using CS.Domain;
+
+namespace CS.Service.Interface
+{
+    public interface IStudentService
+    {
+        void Add(Student student);
+        IEnumerable<Student> GetAll();
+        Student Get(Guid id);
+        void Update(Guid id, Student student);
+        void Delete(Guid id);
+        bool IsCpf(string cpf);
+    }
+}
